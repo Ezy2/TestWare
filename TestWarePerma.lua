3e01f51b0f6ac203de715ab0e8325a024fb147c8
Ezy2 <108997979+Ezy2@users.noreply.github.com>
2022-09-12 20:14:52 -0400

    Update TestWarePerma.lua

--- TestWarePerma.lua
+++ TestWarePerma.lua
@@ -1,1 +1,358 @@
---[[ GameKick local gameid = game.PlaceId if(gameid == 8500690972) or (playerid == 3883018984) then     print("\76\111\97\100\105\110\103\46\46\46") else     game:GetService("\80\108\97\121\101\114\115").LocalPlayer:Kick("\83\117\112\112\111\114\116\101\100\32\71\97\109\101\115\58\32\78\111\32\83\116\111\112\32\79\98\98\121") end ]]-- OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))() local d = OrionLib:MakeWindow({Name = "\84\101\115\116\87\97\114\101\32\86\51", HidePremium = true, SaveConfig = true, ConfigFolder = "\78\111\83\116\111\112", IntroText="\84\101\115\116\87\97\114\101\32\86\51"}) local s = d:MakeTab({ Name = "\77\97\105\110", PremiumOnly = false }) local p = d:MakeTab({ Name = "\67\104\97\114\97\99\116\101\114", PremiumOnly = false }) _G.AJV = 50 s:AddButton({ Name = "\68\101\108\101\116\101\32\77\97\112", Callback = function() for _,m in pairs(Workspace:GetChildren()) do  m:Destroy() end end     }) local g = game:GetService("\85\115\101\114\73\110\112\117\116\83\101\114\118\105\99\101") g.InputBegan:Connect(function(j) if j.UserInputType == Enum.UserInputType.MouseButton1 then if _G.CTP then mouse = game.Players.LocalPlayer:GetMouse() local ha = mouse.Hit+Vector3.new(0,2.5,0) ha = CFrame.new(ha.X,ha.Y,ha.Z) local te = { location1 = Vector3.new(ha.X,ha.Y,ha.Z),  } local mte = game:GetService('TweenService') local im = TweenInfo.new((_G.TWSP),Enum.EasingStyle.Linear) local jim = game.Players.LocalPlayer function t(m) if jim.Character and  jim.Character:FindFirstChild('HumanoidRootPart') then local cj = CFrame.new(m) local dc = mte:Create(jim.Character.HumanoidRootPart,im,{CFrame=cj}) dc:Play() end end t(te.location1) end end end) local g = game:GetService("\85\115\101\114\73\110\112\117\116\83\101\114\118\105\99\101") _G.CTPN = true g.InputBegan:Connect(function(j) if j.UserInputType == Enum.UserInputType.MouseButton1 then if _G.CTPN then mouse = game.Players.LocalPlayer:GetMouse() local _ = mouse.Hit+Vector3.new(0,2.5,0) _ = CFrame.new(_.X,_.Y,_.Z) game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = _ end end end) s:AddToggle({ Name = "\67\108\105\99\107\32\84\80\40\78\111\114\109\97\108\108\41", Default = false, Callback = function(e) _G.CTPN = e end     }) s:AddToggle({ Name = "\67\108\105\99\107\32\84\80\40\84\119\101\101\110\41", Default = false, Callback = function(e) _G.CTP = e end     }) s:AddSlider({ Name = "\84\119\101\101\110\32\83\112\101\101\100", Min = 1, Max = 240, Default = 30, Color = Color3.fromRGB(9, 99, 195), Increment = 0.5, ValueName = "\72\105\103\104\101\114\32\61\32\83\108\111\119\101\114", Callback = function(e) _G.TWSP = e end     }) s:AddTextbox({ Name = "\70\111\114\99\101\32\67\104\97\116", Default = "\77\101\115\115\97\103\101\32\72\101\114\101", TextDisappear = true, Callback = function(e) local n = {[1] = (e),[2] = "\65\108\108",} game:GetService("\82\101\112\108\105\99\97\116\101\100\83\116\111\114\97\103\101").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(n)) end    }) s:AddTextbox({ Name = "\80\108\97\121\101\114\32\84\101\108\101\112\111\114\116\40\78\111\114\109\97\108\108\41", Default = "\80\108\97\121\101\114\32\78\97\109\101", TextDisappear = true, Callback = function(e) game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[e].Character.HumanoidRootPart.CFrame end    }) s:AddTextbox({ Name = "\80\108\97\121\101\114\32\84\101\108\101\112\111\114\116\40\84\119\101\101\110\41", Default = "\80\108\97\121\101\114\32\78\97\109\101", TextDisappear = true, Callback = function(h) game:GetService("\80\108\97\121\101\114\115") local e = game.Players[h].Character.HumanoidRootPart.CFrame local te = { location1 = Vector3.new(e.X, e.Y, e.Z),  } local mte = game:GetService('TweenService') local im = TweenInfo.new((_G.TWTPS),Enum.EasingStyle.Linear) local jim = game.Players.LocalPlayer function t(m) if jim.Character and  jim.Character:FindFirstChild('HumanoidRootPart') then local cj = CFrame.new(m) local dc = mte:Create(jim.Character.HumanoidRootPart,im,{CFrame=cj}) dc:Play() end end t(te.location1) end }) s:AddBind({ Name = "\70\111\114\99\101\32\74\117\109\112", Default = Enum.KeyCode.Space, Hold = false, Callback = function() game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,50,0) end     }) s:AddSlider({ Name = "\80\108\97\121\101\114\32\84\101\108\101\112\111\114\116\40\84\119\101\101\110\41", Min = 1, Max = 240, Default = 10, Color = Color3.fromRGB(9, 99, 195), Increment = 1, ValueName = "\80\108\97\121\101\114\32\84\101\108\101\112\111\114\116\32\83\112\101\101\100\40\84\119\101\101\110\41", Callback = function(e) _G.TWTPS = e end     }) p:AddToggle({ Name = "\71\111\100\32\77\111\100\101\47\87\97\108\107\32\83\112\111\111\102", Default = false, Callback = function(e) _G.GODMODE = e end     }) p:AddToggle({ Name = "\65\110\116\105\32\86\111\105\100\32\40\84\101\108\101\112\111\114\116\41", Default = false, Callback = function(e) _G.SW = e     while _G.SW do if game.Players.LocalPlayer.Character:WaitForChild("\72\117\109\97\110\111\105\100").FloorMaterial ~= Enum.Material.Air then  _G.SP = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame end if game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y < (_G.ATVT) then game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = _G.SP game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,15,0)  wait(0.1) game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(-10,0,-10) *game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.LookVector end wait() end end     }) p:AddSlider({ Name = "\84\114\105\103\103\101\114\32\65\110\116\105\86\111\105\100\40\84\101\108\101\112\111\114\116\41", Min = -1000, Max = 1000, Default = 50, Color = Color3.fromRGB(9, 99, 195), Increment = 1, ValueName = "\65\110\116\105\86\111\105\100\32\72\101\105\103\104\116\40\84\101\108\101\112\111\114\116\41", Callback = function(e) _G.ATVT = e end     }) p:AddToggle({ Name = "\65\110\116\105\32\86\111\105\100\32\40\74\117\109\112\41", Default = false, Callback = function(e) _G.SJ = e     while _G.SJ do if game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y < (_G.ATVH) then game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,(_G.ATV),0) end wait() end end     }) p:AddSlider({ Name = "\84\114\105\103\103\101\114\32\65\110\116\105\86\111\105\100\40\74\117\109\112\41", Min = -1000, Max = 1000, Default = 50, Color = Color3.fromRGB(9, 99, 195), Increment = 1, ValueName = "\65\110\116\105\86\111\105\100\32\72\101\105\103\104\116", Callback = function(e) _G.ATVH = e end     }) p:AddSlider({ Name = "\65\110\116\105\86\111\105\100\32\72\101\105\103\104\116\40\74\117\109\112\41", Min = 50, Max = 500, Default = 50, Color = Color3.fromRGB(9, 99, 195), Increment = 1, ValueName = "\65\110\116\105\86\111\105\100\32\72\101\105\103\104", Callback = function(e) _G.ATV = e end     }) p:AddToggle({ Name = "\86\101\108\111\99\105\116\121\32\83\112\101\101\100", Default = false, Callback = function(e) _G.VSO = e while _G.VSO do game.Players.LocalPlayer.Character:WaitForChild("\72\117\109\97\110\111\105\100\82\111\111\116\80\97\114\116").Velocity = (game.Players.LocalPlayer.Character:WaitForChild("\72\117\109\97\110\111\105\100").MoveDirection*Vector3.new(_G.VS,0,_G.VS)) wait() end end     }) p:AddSlider({ Name = "\86\101\108\111\99\105\116\121\32\83\112\101\101\100", Min = 7.1, Max = 200, Default = 7.1, Color = Color3.fromRGB(9, 99, 195), Increment = 0.5, ValueName = "\86\101\108\111\99\105\116\121\32\83\112\101\101\100", Callback = function(e) _G.VS = e end     }) p:AddToggle({ Name = "\65\117\116\111\32\74\117\109\112", Default = false, Callback = function(e) _G.L = e local sd = false while _G.L do  print(game.Players.LocalPlayer.Character:WaitForChild("\72\117\109\97\110\111\105\100").FloorMaterial) if game.Players.LocalPlayer.Character:WaitForChild("\72\117\109\97\110\111\105\100").FloorMaterial == Enum.Material.Air then if sd then game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,_G.AJV,0) sd = false end else sd = true end wait() end end     }) p:AddSlider({ Name = "\74\117\109\112\72\105\103\104\116\40\97\117\116\111\41", Min = 50, Max = 150, Default = 50, Color = Color3.fromRGB(9, 99, 195), Increment = 1, ValueName = "\65\117\116\111\32\74\117\109\112\72\101\105\103\104\116", Callback = function(e) _G.AJV = e end     }) p:AddButton({ Name = "\83\101\116\32\87\97\108\107\83\112\101\101\100", Callback = function() game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = _G.WSS end }) p:AddSlider({ Name = "\87\97\108\107\32\83\112\101\101\100", Min = 16, Max = 250, Default = 16, Color = Color3.fromRGB(9, 99, 195), Increment = 0.5, ValueName = "\83\112\101\101\100", Callback = function(mt) _G.WSS = mt end     }) local ps = game:GetService("\80\108\97\121\101\114\115").LocalPlayer local a = nil _G.GODMODE = false a = hookmetamethod(game, "\95\95\105\110\100\101\120", function(i, ji) if not checkcaller() and i == ps and ji == "\67\104\97\114\97\99\116\101\114" and _G.GODMODE then return nil end return a(i, ji) end) 
+
+
+ OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+
+-- Windows
+local Window = OrionLib:MakeWindow({Name = "TestWare V3", HidePremium = true, SaveConfig = true, ConfigFolder = "NoStop", IntroText="TestWare V3"})
+
+-- Tabs
+local Main = Window:MakeTab({
+	Name = "Main",
+	PremiumOnly = false
+})
+
+local Character = Window:MakeTab({
+	Name = "Character",
+	PremiumOnly = false
+})
+-- Global Values
+
+_G.AJV = 50
+
+
+-- Main
+
+Main:AddButton({
+	Name = "Delete Map",
+	Callback = function()
+    for _,v in pairs(Workspace:GetChildren()) do 
+        v:Destroy()
+        end
+  	end    
+})
+
+local dwUIS = game:GetService("UserInputService")
+
+dwUIS.InputBegan:Connect(function(i)
+    if i.UserInputType == Enum.UserInputType.MouseButton1 then
+        if _G.CTP then
+            mouse = game.Players.LocalPlayer:GetMouse()
+            local pos = mouse.Hit+Vector3.new(0,2.5,0)
+            pos = CFrame.new(pos.X,pos.Y,pos.Z)
+            local teleport_table = {
+                location1 = Vector3.new(pos.X,pos.Y,pos.Z), -- your desired position
+            }
+
+            local tween_s = game:GetService('TweenService')
+            local tweeninfo = TweenInfo.new((_G.TWSP),Enum.EasingStyle.Linear)
+
+            local lp = game.Players.LocalPlayer
+            
+            function bypass_teleport(v)
+                if lp.Character and 
+                lp.Character:FindFirstChild('HumanoidRootPart') then
+                    local cf = CFrame.new(v)
+                    local a = tween_s:Create(lp.Character.HumanoidRootPart,tweeninfo,{CFrame=cf})
+                    a:Play()
+                    -- a.Completed:Wait()
+                    -- print('Teleporting Done!')
+                end
+            end
+            
+            bypass_teleport(teleport_table.location1)
+        end
+    end
+end)
+
+local dwUIS = game:GetService("UserInputService")
+_G.CTPN = true
+
+dwUIS.InputBegan:Connect(function(i)
+    if i.UserInputType == Enum.UserInputType.MouseButton1 then
+        if _G.CTPN then
+            mouse = game.Players.LocalPlayer:GetMouse()
+            local poss = mouse.Hit+Vector3.new(0,2.5,0)
+            poss = CFrame.new(poss.X,poss.Y,poss.Z)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = poss
+        end
+    end
+end)
+
+Main:AddToggle({
+	Name = "Click TP(Normall)",
+	Default = false,
+	Callback = function(Value)
+		_G.CTPN = Value
+	end    
+})
+
+Main:AddToggle({
+	Name = "Click TP(Tween)",
+	Default = false,
+	Callback = function(Value)
+		_G.CTP = Value
+	end    
+})
+
+Main:AddSlider({
+	Name = "Tween Speed",
+	Min = 1,
+	Max = 240,
+	Default = 30,
+	Color = Color3.fromRGB(9, 99, 195),
+	Increment = 0.5,
+	ValueName = "Higher = Slower",
+	Callback = function(Value)
+		_G.TWSP = Value
+	end    
+})
+
+Main:AddTextbox({
+	Name = "Force Chat",
+	Default = "Message Here",
+	TextDisappear = true,
+	Callback = function(Value)
+        local args = {[1] = (Value),[2] = "All",}
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+	end	  
+})
+
+Main:AddTextbox({
+	Name = "Player Teleport(Normall)",
+	Default = "Player Name",
+	TextDisappear = true,
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[Value].Character.HumanoidRootPart.CFrame
+	end	  
+})
+
+Main:AddTextbox({
+	Name = "Player Teleport(Tween)",
+	Default = "Player Name",
+	TextDisappear = true,
+	Callback = function(Valuee)
+	game:GetService("Players")
+	local Value = game.Players[Valuee].Character.HumanoidRootPart.CFrame
+	
+		local teleport_table = {
+            location1 = Vector3.new(Value.X, Value.Y, Value.Z), -- your desired position
+            }
+
+            local tween_s = game:GetService('TweenService')
+            local tweeninfo = TweenInfo.new((_G.TWTPS),Enum.EasingStyle.Linear)
+
+            local lp = game.Players.LocalPlayer
+            
+            function bypass_teleport(v)
+                if lp.Character and 
+                lp.Character:FindFirstChild('HumanoidRootPart') then
+                    local cf = CFrame.new(v)
+                    local a = tween_s:Create(lp.Character.HumanoidRootPart,tweeninfo,{CFrame=cf})
+                    a:Play()
+                end
+            end
+            
+            bypass_teleport(teleport_table.location1)
+        end
+})
+
+Main:AddBind({
+	Name = "Force Jump",
+	Default = Enum.KeyCode.Space,
+	Hold = false,
+	Callback = function()
+	game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,50,0)
+end    
+})
+
+Main:AddSlider({
+	Name = "Player Teleport(Tween)",
+	Min = 1,
+	Max = 240,
+	Default = 10,
+	Color = Color3.fromRGB(9, 99, 195),
+	Increment = 1,
+	ValueName = "Player Teleport Speed(Tween)",
+	Callback = function(Value)
+    	_G.TWTPS = Value
+	end    
+})
+
+-- Character
+
+Character:AddToggle({
+	Name = "God Mode/Walk Spoof",
+	Default = false,
+	Callback = function(Value)
+		_G.GODMODE = Value
+	end    
+})
+
+Character:AddToggle({
+	Name = "Anti Void (Teleport)",
+	Default = false,
+	Callback = function(Value)
+	_G.SW = Value    
+	    
+		while _G.SW do
+	    if game.Players.LocalPlayer.Character:WaitForChild("Humanoid").FloorMaterial ~= Enum.Material.Air then 
+	        _G.SP = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame end
+		    if game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y < (_G.ATVT) then
+		        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = _G.SP
+		        game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,15,0) 
+                wait(0.1)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(-10,0,-10) *game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.LookVector
+		        end
+		    wait()
+		    end
+	end    
+})
+
+Character:AddSlider({
+	Name = "Trigger AntiVoid(Teleport)",
+	Min = -1000,
+	Max = 1000,
+	Default = 50,
+	Color = Color3.fromRGB(9, 99, 195),
+	Increment = 1,
+	ValueName = "AntiVoid Height(Teleport)",
+	Callback = function(Value)
+    	_G.ATVT = Value
+	end    
+})
+
+Character:AddToggle({
+	Name = "Anti Void (Jump)",
+	Default = false,
+	Callback = function(Value)
+	_G.SJ = Value    
+	    
+		while _G.SJ do
+		    if game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y < (_G.ATVH) then
+		        game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,(_G.ATV),0)
+		        end
+		    wait()
+		    end
+	end    
+})
+
+Character:AddSlider({
+	Name = "Trigger AntiVoid(Jump)",
+	Min = -1000,
+	Max = 1000,
+	Default = 50,
+	Color = Color3.fromRGB(9, 99, 195),
+	Increment = 1,
+	ValueName = "AntiVoid Height",
+	Callback = function(Value)
+    	_G.ATVH = Value
+	end    
+})
+
+
+Character:AddSlider({
+	Name = "AntiVoid Height(Jump)",
+	Min = 50,
+	Max = 500,
+	Default = 50,
+	Color = Color3.fromRGB(9, 99, 195),
+	Increment = 1,
+	ValueName = "AntiVoid Heigh",
+	Callback = function(Value)
+    	_G.ATV = Value
+	end    
+})
+
+
+Character:AddToggle({
+	Name = "Velocity Speed",
+	Default = false,
+	Callback = function(Value)
+	    _G.VSO = Value
+	    
+	    while _G.VSO do
+	        game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Velocity = (game.Players.LocalPlayer.Character:WaitForChild("Humanoid").MoveDirection*Vector3.new(_G.VS,0,_G.VS))
+	    wait()
+	    end
+	end    
+})
+
+Character:AddSlider({
+	Name = "Velocity Speed",
+	Min = 7.1,
+	Max = 200,
+	Default = 7.1,
+	Color = Color3.fromRGB(9, 99, 195),
+	Increment = 0.5,
+	ValueName = "Velocity Speed",
+	Callback = function(Value)
+    	_G.VS = Value
+	end    
+})
+
+Character:AddToggle({
+	Name = "Auto Jump",
+	Default = false,
+	Callback = function(Value)
+	    _G.L = Value
+	    local lg = false
+	    
+		while _G.L do 
+		    print(game.Players.LocalPlayer.Character:WaitForChild("Humanoid").FloorMaterial)
+		    if game.Players.LocalPlayer.Character:WaitForChild("Humanoid").FloorMaterial == Enum.Material.Air then
+		        if lg then
+		        game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,_G.AJV,0)
+		        lg = false end
+		    else
+		        lg = true
+		        end
+		    wait()
+		    end
+	end    
+})
+
+Character:AddSlider({
+	Name = "JumpHight(auto)",
+	Min = 50,
+	Max = 150,
+	Default = 50,
+	Color = Color3.fromRGB(9, 99, 195),
+	Increment = 1,
+	ValueName = "Auto JumpHeight",
+	Callback = function(Value)
+		_G.AJV = Value
+	end    
+})
+
+Character:AddButton({
+	Name = "Set WalkSpeed",
+	Callback = function()
+	        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = _G.WSS
+	        end
+})
+
+Character:AddSlider({
+	Name = "Walk Speed",
+	Min = 16,
+	Max = 250,
+	Default = 16,
+	Color = Color3.fromRGB(9, 99, 195),
+	Increment = 0.5,
+	ValueName = "Speed",
+	Callback = function(Speed)
+		_G.WSS = Speed
+	end    
+})
+
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local OldIndex = nil
+_G.GODMODE = false
+
+OldIndex = hookmetamethod(game, "__index", function(Self, Key)
+    if not checkcaller() and Self == LocalPlayer and Key == "Character" and _G.GODMODE then
+        return nil
+    end
+
+    return OldIndex(Self, Key)
+end)
